2cc164fe08bc027e01308790416918ca5d47eeac
Alfonso Casanova <fonycas@hotmail.com>
2023-08-05 19:23:54 +0200

    first commit
    unreal thingies

---
+++ Source/MagicWithin/MagicWithinGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MagicWithinGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAGICWITHIN_API AMagicWithinGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
